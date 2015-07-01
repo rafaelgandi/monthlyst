e32750ec12b1c3763aa2ab8149a24e7f5e52271f
Raffy <rafael@sushidigital.com.au>
2015-07-01 08:41:17 +0800

    minor updates to the list page month label button

--- js/m.Config.js
+++ js/m.Config.js
@@ -3,7 +3,7 @@ navigator.define('m\Config', function (z, undefined) {
 		appName: 'Monthlyst',
 		appAuthor: 'Rafael Gandionco',
 		appVersion: '1.0',
-		appBuild: '06262015',
+		appBuild: '07012015',
 		appYear: '2015',
 		quarterlyMonths: [3, 6, 9, 12],
 		backupFilename: 'monthlyst.txt',
